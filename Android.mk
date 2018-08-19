omnirom/android_hardware_qcom_display

display: add CAF vs. AOSP hal separation ifdef
Change-Id: I466abcbf17f27a462491d2f3babf9bbd48802029

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
 ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8084 based platforms
     include $(call all-named-subdir-makefiles,msm8084)
@@ -11,6 +12,7 @@ ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
 else
     #This is for 8960 based platforms
     include $(call all-named-subdir-makefiles,msm8960)
+endif
 endif
 endif
 endif

Colt-Enigma/platform_hardware_qcom_display

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 LOCAL_PATH := $(call my-dir)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
@@ -18,4 +19,5 @@ else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
 endif
 
+endif
 endif